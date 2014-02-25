3c499fe446169e1a6fa3e796807920aabae85b82
Brad Lowenstein <blowenstein93@gmail.com>
2014-02-25 15:54:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linekiller::Application.config.secret_key_base = 'b486afbc8657ba85177730f0ee2b1e28f6d2d97b4bfdc60e60cb6aca32bf1200a1675c09e3d6c49a2a52c81cf736b6c3690df11654ecd96cc2d8f9705418437a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Linekiller::Application.config.session_store :cookie_store, key: '_linekiller_session'
